6c69d5995606c95364b4d3bf516b1a3d94614f32
Daniel Hammarström <daniel.hammarstrom@inn.no>
2025-03-25 15:19:47 +0100

    upd w seqwrap compose and check

--- R/seqwrap.R
+++ R/seqwrap.R
@@ -1,10 +1,30 @@
-# Define a custom property type that accepts either NULL or a list
+# Defines custom property types for use in class constructors #
+
+
+# Define a modelfun property that correctly updates model_print
+modelfun_prop <- S7::new_property(
+  validator = function(value) {
+    if (!(is.null(value) || is.function(value))) "must be NULL or a function"
+  }
+)
+
+# Accepts either NULL or a list, this will also validate data frames.
 null_or_list <- S7::new_property(
   # The validator function checks if the value is NULL or a list
   validator = function(value) {
     if (!(is.null(value) || is.list(value))) "must be NULL or a list"
   }
 )
+
+# Define a custom property type that accepts either NULL or a list
+null_or_df <- S7::new_property(
+  # The validator function checks if the value is NULL or a list
+  validator = function(value) {
+    if (!(is.null(value) || is.data.frame(value)))
+      "must be NULL or a data.frame"
+  }
+)
+
 
 # Define a custom property type that accepts either data frame or a list
 df_or_list <- S7::new_property(
@@ -16,7 +36,36 @@ df_or_list <- S7::new_property(
   }
 )
 
+# Define a call property for use in the swcontainer function
+call_prop <- S7::new_property(
+  validator = function(value) {
+    if (!(is.call(value) || is.null(value))) {
+      return("must be a language object representation of a call")
+    }
+    return(NULL) # Explicitly return NULL for valid values
+  }
+)
 
+
+
+
+
+
+
+#' seqwrapResults class
+#'
+#' seqwrapResults constructor function.
+#'
+#' @slot models description
+#' @slot summaries description
+#' @slot evaluations description
+#' @slot errors description
+#' @slot n description
+#' @slot k description
+#' @slot call_arguments description
+#' @slot call_engine description
+#'
+#'
 #' @export
 seqwrapResults <- S7::new_class(
   name = "seqwrap_results",
@@ -33,21 +82,41 @@ seqwrapResults <- S7::new_class(
   )
 )
 
-
+#' seqwrap container class
+#'
+#' The seqwrap container is used to store and validate data input
+#' to the to seqwrap.
+#'
+#' @slot modelfun A function used to fit models using data and meta data
+#' as input.
+#' @slot arguments A list of arguments provided to the fitting function.
+#' @slot data A data frame or a list of data frames
+#' @slot rownames A logical indicating if row names should be used as target id
+#' @slot metadata A data frame with sample information
+#' @slot targetdata A data frame with sample information
+#' @slot samplename A character for sample name identification
+#' @slot additional_vars A character vector of additional variables exported to
+#' @slot summary_fun A function
+#' @slot eval_fun A function
+#' @slot exported A list
+#'
 #' @export
 swcontainer <- S7::new_class(
   name = "swcontainer",
   parent = S7::class_list,
   properties = list(
-    fitting_fun = S7::class_function,
+    modelfun = modelfun_prop,
     arguments = null_or_list,
-    data = df_or_list,
+    data = null_or_list,
+    rownames = S7::class_logical,
     metadata = S7::class_data.frame,
+    targetdata = null_or_list,
     samplename = S7::class_character,
     additional_vars = S7::class_character,
     summary_fun = S7::class_function,
     eval_fun = S7::class_function,
-    exported = S7::class_list
+    exported = S7::class_list,
+    model_print = S7::class_character
   )
 )
 
@@ -110,8 +179,8 @@ S7::method(print, seqwrapResults) <- function(x) {
 #'
 #' @param x An optional named list or DGEList object (DESeqDataSet not yet
 #' implemented).
-#' @param fitting_fun A model fitting function like stats::lm,
-#' glmmTMB::glmmTMB, lme4::lmer
+#' @param modelfun A model fitting function like stats::lm,
+#' glmmTMB::glmmTMB or lme4::lmer
 #' @param arguments An alist or list of arguments to be passed to the fitting
 #'  function, this should not contain data. Note that the formula must have
 #'  y as the dependent variable.
@@ -143,20 +212,37 @@ S7::method(print, seqwrapResults) <- function(x) {
 #' @param exported A list of functions, values etc. to be passed to
 #' summary_fun and eval_fun. This list must contain any functions that
 #' should be used in model summarise or evaluations.
+#' @param update A list of named parameters to update a swcontainer object.
 #' @return A swcontainer object for direct use in seqwrap.
 #' @export
 seqwrap_compose <- function(
-  x,
-  fitting_fun,
+  x = NULL,
+  modelfun,
   arguments,
   data,
+  rownames = FALSE,
   metadata,
+  targetdata,
   samplename = "seq_sample_id",
   additional_vars = NULL,
   summary_fun = NULL,
   eval_fun = NULL,
-  exported = list()
+  exported = list(),
+  update = list()
 ) {
+
+  # Check if x is a swcontainer for updating
+  if (S7::S7_inherits(x, swcontainer)) {
+    # Pass updates as a list for the named parameter
+    return(seqwrap_update(container = x, update))
+  }
+
+
+  # Extract the modelling algorithm for printing
+  if (!is.null(modelfun)) {
+    call_str <- match.call()
+  }
+
   if (class(x) == "DGEList") {
     # Prepare count data from DGEList
 
@@ -182,10 +268,6 @@ seqwrap_compose <- function(
       if (!is.null(samplename)) colnames(xdata)[1] <- samplename
     }
 
-
-
-
-
     # Prepare metadata from DGEList
 
     xmetadata <- data.frame(
@@ -202,9 +284,237 @@ seqwrap_compose <- function(
     )
   }
 
+  # Initialize an empty container and add objects if they exist
   container <- swcontainer()
+
+
+
+  # Set other properties
+  if (!is.null(modelfun)) container@modelfun <- modelfun
+  if (!is.null(modelfun)) container@model_print <- deparse(call_str$modelfun)
+  if (!is.null(arguments)) container@arguments <- arguments
+  if (!is.null(data)) container@data <- data
+  if (!is.null(rownames)) container@rownames <- rownames
+  if (!is.null(metadata)) container@metadata <- metadata
+  if (!is.null(targetdata)) container@targetdata <- targetdata
+  if (!is.null(samplename)) container@samplename <- samplename
+  if (!is.null(additional_vars)) container@additional_vars <- additional_vars
+  if (!is.null(summary_fun)) container@summary_fun <- summary_fun
+  if (!is.null(eval_fun)) container@eval_fun <- eval_fun
+  if (!is.null(exported)) container@exported <- exported
+
+  # Return the populated container
+  return(container)
 }
 
+
+# Define a generic with named parameters only
+seqwrap_update <- S7::new_generic("seqwrap_update", "container")
+
+# Implement method for swcontainer class
+S7::method(seqwrap_update, swcontainer) <- function(container, update = list()) {
+
+  if (!S7::S7_inherits(container, swcontainer)) {
+    stop("First argument must be a swcontainer object")
+  }
+
+
+  if (length(update) == 0) {
+    return(container) # Nothing to update
+  }
+
+
+  # Get property names
+ name <- names(update)
+
+
+ # Special handling for modelfun to update model_print
+  if ("modelfun" %in% name)  {
+
+    # The model print information is not updated
+    # due to difficulties getting the call from the
+    # method. This is a temporary solution.
+    update$model_print <- "The model function has been updated"
+
+     }
+
+  # Update the container object
+  S7::props(container) <- update
+
+ return(container)
+}
+
+
+#' Check swcontainer objects for seqwrap
+#'
+#' This function performs verbose checks/diagnostics of a swcontainer object
+#'
+#' @param x A swcontainer object
+#' @return A list of diagnostics
+seqwrap_check <- function(x, verbose = TRUE) {
+  # Check if the container is a swcontainer object
+  if (!S7::S7_inherits(container, swcontainer)) {
+    stop("The container must be a swcontainer object")
+  }
+
+
+
+  # checks if arguments for the provided fitting function matches arguments
+  if (!all(names(x@arguments) %in% names(formals(x@modelfun)))) {
+    cli::cli_abort(
+      "Arguments do not match named arguments of the selected
+                   \nmodel fitting function ('modelfun')."
+    )
+  }
+
+  # Check if the data has a character vector for first column
+  # (indicating transcript id).
+  if (!is.character(x@data[, 1])) {
+    cli::cli_abort(
+      "The first column of the data is not character or
+            factor,\ncheck if this column indicate target
+            identifications."
+    )
+  }
+
+  # Check if the sample name is present in the meta data
+  if (!x@samplename %in% colnames(x@metadata)) {
+    cli::cli_abort(
+      "The samplename does not exist in the metadata,\nno
+            variable for matching metadata and
+            data."
+    )
+  }
+
+  # Check that data is formatted correctly
+  if (!all(x@metadata[, x@samplename] %in% names(x@data[, -1]))) {
+    cli::cli_abort(
+      "The sample names in the metadata does
+         not match the sample column names in the seqdata."
+    )
+  }
+
+
+  # Count the number of unique targets in the data
+  if (x@rownames) {
+    n_targets <- length(unique(rownames(x@data)))
+  } else {
+    n_targets <- length(unique(x@data[,1]))
+  }
+
+  # If the target unique indicator is not the same as number of rows abort
+  if (!n_targets == nrow(x@data)) {
+    cli::cli_abort(
+      "The number of unique target identifications does not
+        match the number of rows in the data. You might have duplicate id's."
+    )
+  }
+
+  # Count the number of samples
+  n_samples <- length(unique((x@metadata[, x@samplename])))
+
+  # If the unique sample indicator is not the same as number of rows abort
+  if (!n_samples == nrow(x@metadata)) {
+    cli::cli_abort(
+      "The number of unique sample identifications does not
+        match the number of rows in the meta data.
+      You might have duplicate sample id's."
+    )
+  }
+
+  # Check that sample id match between data and meta data
+  meta_data_sample_id <- unique((x@metadata[, x@samplename]))
+
+  if (x@rownames) {
+    data_sample_id <- colnames(x@data)
+  } else {
+    data_sample_id <- colnames(x@data[, -1])
+  }
+
+  # Save sample id for printing
+  if (length(data_sample_id) > 3) {
+    sample_id_print <- paste(data_sample_id[1:3], collapse = ", ")
+  } else {
+    sample_id_print <- paste0(paste(data_sample_id, collapse = ", "), ", ...")
+  }
+
+
+  if (!all(meta_data_sample_id %in% data_sample_id) &&
+      all(data_sample_id %in% meta_data_sample_id)) {
+    cli::cli_abort(
+    "The sample names in the meta data does not match the sample column names
+    in the data. Check if the sample names are correctly formatted and that
+    `samplename` looks for the correct variable in the meta data."
+    )
+  }
+
+  # If verbose is TRUE, print the diagnostics
+  if(verbose) {
+
+    printfun <- function() {
+
+      cli::cli_h1("seqwrap diagnostics")
+
+      cli::cli_inform("The swcontainer object has been checked for
+                      consistency with the following diagnostics:\n")
+
+      cli::cli_ul(
+        "The number of unique target identifications is {n_targets}."
+      )
+
+      cli::cli_ul(
+        "The number of unique sample identifications is {n_samples}."
+      )
+
+      cli::cli_ul(
+        "Sample identifications ({sample_id_print}) match between data
+        and meta data."
+      )
+
+      cli::cli_ul(
+        "The modelling function to be used is {x@model_print}."
+      )
+
+
+    }
+    printfun()
+  }
+
+  # If the swcontainer is a fully populated data container return TRUE
+  return(TRUE)
+
+}
+
+
+
+
+
+# #' Print method for objects of class swcontainer
+# #'
+# #' @description
+# #' Invoking the print method on swcontainer gives a summary
+# #' of data container.
+# #'
+# #' @param x A swcontainer object
+# #'
+# #' @return Invisibly returns the object
+# #'
+# #' @examples
+# #' seqwrap.dat <- seqwrap_compose(...)
+# #' print(seqwrap.dat)
+# #'
+# #' @method print swcontainer
+# #' @name print.swcontainer
+# S7::method(print, swcontainer) <- function(x) {
+#   cli::cli_h1("seqwrap data container")
+#
+#   if (!is.null(x@fitting_fun)) {
+#
+#   }
+#
+#
+#  # invisible(x)
+# }
 
 #' A flexible upper-level wrapper for iterative modelling using any available
 #' fitting algorithm.
@@ -235,7 +545,7 @@ seqwrap_compose <- function(
 #' @export
 seqwrap <- function(
   y,
-  fitting_fun,
+  modelfun,
   arguments,
   data,
   metadata,
@@ -250,53 +560,12 @@ seqwrap <- function(
   subset = NULL,
   cores
 ) {
-  ## Prepare data #####
-  data <- data.frame(data)
-  metadata <- data.frame(metadata)
+  #
 
-  ## Subset the data
-  if (!is.null(subset)) data <- data[subset, ]
 
-  ## Sanity checks
 
-  # checks if arguments for the provided fitting function matches arguments
-  if (!all(names(arguments) %in% names(formals(fitting_fun)))) {
-    cli::cli_abort(
-      "Arguments do not match named arguments of the selected
-                   \nmodel fitting function (fitting_fun).",
-      call = call
-    )
-  }
 
-  # Check if the data has a character vector for first column
-  # (indicating transcript id).
-  if (!is.character(data[, 1])) {
-    cli::cli_abort(
-      "The first column of the data is not character or
-            factor,\ncheck if this column indicate target
-            identifications.",
-      call = call
-    )
-  }
 
-  # Check if the sample name is present in the meta data
-  if (!samplename %in% colnames(metadata)) {
-    cli::cli_abort(
-      "The samplename does not exist in the metadata,\nno
-            variable for matching metadata and
-            data.",
-      call = call
-    )
-  }
-
-  # Check that data is formatted correctly
-  if (!all(metadata[, samplename] %in% names(data[, -1]))) {
-    cli::cli_abort(
-      "The sample names in the metadata does
-         not match the sample column names in the seqdata.",
-      call = call
-    )
-  }
 
   # data_helper function. Combine data into a list of data frames
   # containing variables, y in case of user-provided data frame;
@@ -320,8 +589,8 @@ seqwrap <- function(
   ## Arguments to string
   call_arguments <- deparse(funcall$arguments)
   call_arguments <- sub("^.*?\\((.*)\\).*$", "\\1", call_arguments)
-  ## fitting_fun to string
-  call_engine <- deparse(funcall$fitting_fun)
+  ## modelfun to string
+  call_engine <- deparse(funcall$modelfun)
 
   # Print pre-fit information
   cli::cli_h1("seqwrap")
@@ -341,10 +610,9 @@ seqwrap <- function(
     varlist = c(
       "metadata",
       "arguments",
-      "fitting_fun",
+      "modelfun",
       "samplename",
       "additional_vars",
-      "fitting_fun",
       "save_models",
       "exported",
       "model_path",
@@ -367,7 +635,7 @@ seqwrap <- function(
     add_vars = additional_vars,
     mt_summary_fun = summary_fun,
     mt_eval_fun = eval_fun,
-    ffun = fitting_fun,
+    ffun = modelfun,
     return_mod = return_models,
     save_mods = save_models,
     mod_path = model_path
