52e73163de44f08ba21374be53b6ac86f9a193ce
Daniel Hammarström <daniel.hammarstrom@inn.no>
2025-10-22 13:20:29 +0200

    comments on simcounts2 tests

--- tests/testthat/test-simcounts2.R
+++ tests/testthat/test-simcounts2.R
@@ -1,36 +1,49 @@
 
 library(testthat)
+library(seqwrap)
 
 
 
 test_that("simcounts2 returns the documented structure with defaults", {
   skip_on_cran() # simulation thus no need to be checked by CRAN
+
+  #### Use variables so that we know nrows, n1 n2 etc?
+
+
   out <- simcounts2()
 
   # The documented return is a list with (1) counts, (2) eta, (3) phi, (4) meta
   expect_type(out, "list")
-  expect_true(all(c("counts", "eta", "phi", "meta") %in% names(out)))
+  expect_true(all(c("counts", "eta", "phi", "metadata") %in% names(out)))
 
   # counts: matrix of non-negative integers; dimensions: genes x samples
-  expect_true(is.matrix(out$counts))
-  expect_true(is.numeric(out$counts))
-  expect_true(all(is.finite(out$counts)))
+  expect_true(is.data.frame(out$counts)) #### Re-write with  expect_s3_class
+  expect_true(is.numeric(out$counts[,2]))     ########### Re-write with apply
+  expect_true(all(is.finite(out$counts))) ### Re-write with apply
   expect_true(all(out$counts >= 0))
 
 
   # allow >=0 and integer-valued
-  expect_true(all(out$counts == floor(out$counts)))
+  expect_true(all(as.matrix(out$counts[,-1]) == floor(as.matrix(out$counts[,-1]))))
 
   # eta: matrix;
-  expect_true(is.matrix(out$eta))
+  expect_true(is.data.frame(out$eta))
+  # Check that eta and counts have the same dimensions
   expect_equal(dim(out$eta), dim(out$counts))
-  expect_true(is.numeric(out$eta))
-  expect_true(all(is.finite(out$eta)))
+  # Check that all eta values are numeric
+  expect_true(all(apply(out$eta[,-1], 2, is.numeric)))
+  # Check tha no values are missing or inf
+  expect_true(all(apply(out$eta[,-1], 2, is.finite)))
 
-  # phi: numeric vector
-  expect_true(is.numeric(out$phi))
-  expect_true(all(is.finite(out$phi)))
+
+  # phi: numeric vector (second column) in a data frame,
+  expect_true(is.numeric(out$phi[,2]))
+  expect_true(all(is.finite(out$phi[,2])))
+  # Check that number of columns are greater than 0
   expect_gt(length(out$phi), 0)
+  # Check number of rows
+  expect_gt(nrow(out$phi), 0)
+
 
   # meta: data.frame with one row per sample
   expect_s3_class(out$meta, "data.frame")
